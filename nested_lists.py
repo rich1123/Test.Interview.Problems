0782b84fb8e1176314aea4d988a99ccc798ca3fb
Rich Maiale <richm1123@gmail.com>
2020-05-06 11:44:28 -0400

    nested lists solved

--- nested_lists.py
+++ nested_lists.py
@@ -42,9 +42,33 @@ There are 5 students in this class whose names and grades are assembled to build
 python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 
 The lowest grade of 37.2 belongs to Tina. The second lowest grade of 37.21 belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line."""
-
-
+nested_l = []
+scores = []
 if __name__ == '__main__':
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        nested_l += [[name, score]]
+        scores += [score]
+    s_2 = sorted(set(scores))[1]
+
+    for n, s in sorted(nested_l):
+        if s == s_2:
+            print(n)
+
+
+
+"""below code doesn't work, I still dont know why cause it 
+debugs to the point where the final if before printing the answer is equal"""
+    # nested_l = []
+    # if __name__ == '__main__':
+    #     for _ in range(int(input())):
+    #         name = input()
+    #         score = float(input())
+    #         nested_l += [[name, score]]
+    #     s_2 = nested_l[1]
+    #     # scores = [score]
+    #
+    #     for name, score in sorted(nested_l):
+    #         if score == s_2:
+    #             print(name)
