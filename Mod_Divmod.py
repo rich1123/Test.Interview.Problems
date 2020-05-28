f9d68e86d2c9621692be0d98869e053a182b589a
Rich Maiale <richm1123@gmail.com>
2020-05-28 09:40:38 -0400

    new answer

---
+++ Mod_Divmod.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    # def func(a,b):
+    div = (a//b)
+    mod = (a%b)
+    div_mod = (div, mod)
+    print(f"{div}\n{mod}\n{div_mod}")
